f429efe8a0c65a3249e920191c97f4a02aa31756
Irvin Fiz <irvin.fiz.dev@gmail.com>
2021-01-28 15:49:07 -0600

    cors

--- src/index.js
+++ src/index.js
@@ -10,6 +10,19 @@ const PORT = api.port
 const app = express()
 app.use(cors())
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  )
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+  next()
+})
+
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello World'
